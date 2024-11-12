a2e1aa23220005820dc7aebdf057e8a43f82d505
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-11-12 16:15:04 +0000

    Added mondoDB.js database connection

---
+++ Public/Javascript/mongoDB.js
@@ -0,0 +1,22 @@
+// Initialisation for properties-reader for MongoDB Atlas
+
+let propertiesReader = require("properties-reader");
+const path = require("path");
+let propertiesPath = path.resolve(__dirname, "../../conf/db.properties");
+let properties = propertiesReader(propertiesPath);
+let dbPprefix = properties.get("db.prefix");
+
+//URL-Encoding of User and PWD
+//for potential special characters
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
+
+module.exports = db; // Export the db instance for use in server.js
