1edb35cdb76f599aed71f570e1fa408de02c69c8
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-11-12 16:22:07 +0000

    Deleted notes.txt, updated script.js and .json files

--- Public/Javascript/script.js
+++ Public/Javascript/script.js
@@ -7,13 +7,7 @@ const shoppingPage = document.getElementById("shoppingPage");
 const headerContainer = document.getElementById("headerContainer");
 const checkoutPage = document.getElementById("checkoutPage");
 
-// This function adds the ability for the user to scroll to the top of the page instantly rather than scrolling up manually.
-scrollToTop.addEventListener("click", function (e) {
-  e.preventDefault(); // Prevent the URL being updated or page refresh.
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
-// This function toggles the display of the Shopping Page section to 'block', making it visible.
+// This function toggles the display of the Shopping Page section to 'block', making it visible. Need to change to vue.js code
 function toggleShoppingPage() {
   landingPage.style.display = "none";
   checkoutPage.style.display = "none";
@@ -21,7 +15,7 @@ function toggleShoppingPage() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-// This function toggles the display of the About Us Page section to 'block', making it visible.
+// This function toggles the display of the About Us Page section to 'block', making it visible. Need to change to vue.js code
 function toggleAboutUs() {
   shoppingPage.style.display = "none";
   checkoutPage.style.display = "none";
@@ -29,7 +23,7 @@ function toggleAboutUs() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-// This function toggles the display of the checkout Page section to 'block', making it visible.
+// This function toggles the display of the checkout Page section to 'block', making it visible. Need to change to vue.js code
 function toggleCheckoutPage() {
   shoppingPage.style.display = "none";
   landingPage.style.display = "none";
